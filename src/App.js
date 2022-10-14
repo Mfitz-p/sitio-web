0dde4568841f61198dce76564b75de68255edcd5
root <root@LAPTOP-2IIG43KR.localdomain>
2022-10-14 09:58:38 -0500

    Se modificó componente app.js para evitar conflicto entre ramas

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-<<<<<<< HEAD
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 import Navbar from './Components/Navbar';
-// import Button from 'react-bootstrap/Button';
+import Footer from './Components/Footer';
+//import Nosotros from './Views/Nosotros';
+//import Pagina404 from './Views/Pagina404';
+
 
 function App() {
   return (
     <>
    <Navbar/>
+   <Footer/>
    {/* <Button variant="success">Success</Button> */}
     </>
   );
 }
 
 export default App;
-=======
-import Footer from './Components/Footer';
-//import Nosotros from './Views/Nosotros';
-//import Pagina404 from './Views/Pagina404';
 
-function App() {
-  return (
-    <>
-  
-    <Footer/>
-    </>
-  );
-}
 
-export default App;
->>>>>>> e554dbb47566e68023437aeb47201876f0ed57ee
