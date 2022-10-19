5152048c269609f6df0b815009de2a822a18ae54
root <root@LAPTOP-2IIG43KR.localdomain>
2022-10-18 19:48:21 -0500

    Agregar y modificar  propiedades responsivas a Contacto y Nosotros

--- src/Views/Pagina404.js
+++ src/Views/Pagina404.js
@@ -1,17 +1,17 @@
 /**
  * Página de error 404
  */
-//import lupa from '../images/lupaCompleta404.png';
+import {  Container } from 'react-bootstrap/'
 import "../App.css";
 function Pagina404() {
   return (
-    <>
-          <div className='fondo' style={{ margin: "4.6rem" }}>
-              <h1 style={{ fontSize: "15rem" }}>404</h1>
-              <div style={{ fontSize: "7rem" }}>Página no encontrada</div>
-              <p>La página que solicitaste no pudo ser encontrada</p>
+    <Container fluid>
+          <div className='fondo' >
+              <h1 style={{ fontSize: "clamp(77px, 13vw, 480px)", margin:"50px" }}>404 ❌</h1>
+              <div style={{ fontSize: "clamp(60px, 7vw, 480px)" }}>Página no encontrada</div>
+              <p style={{ fontSize: "40px", margin:"1%" }}>La página que solicitaste no pudo ser encontrada</p>
           </div>
-    </>
+    </Container>
   )
 }
 
