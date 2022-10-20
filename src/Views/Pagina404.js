4f983dd06e1b76a6cfdc670cf27da058c9e027e0
root <root@LAPTOP-2IIG43KR.localdomain>
2022-10-20 17:46:10 -0500

    Modificación del espacio del input mensaje y organización de las mediaquery en CSS

--- src/Views/Pagina404.js
+++ src/Views/Pagina404.js
@@ -1,19 +1,19 @@
 /**
  * Página de error 404
  */
-import {  Container } from 'react-bootstrap/'
+import { Container } from 'react-bootstrap/'
 import "../App.css";
 function Pagina404() {
   return (
     <Container fluid >
-          <div className='fondo ' >
-              <h1 className='text404' style={{ fontSize: "clamp(130px, 13vw, 480px)"}}>404 
-                <div className='d-none d-md-inline' >❌</div>
-                <div className=' d-sm-inline d-md-none'style={{ textAlign:'center'}} >🔎</div>
-              <div style={{ fontSize: "clamp(50px, 6vw, 200px)" }}>Página no encontrada</div>
-              <p style={{ fontSize: "clamp(20px, 2.4vw, 200px)" }}>La página que solicitaste no pudo ser encontrada</p>
-              </h1>
-          </div>
+      <div className='fondo ' >
+        <h1 className='text404' style={{ fontSize: "clamp(130px, 13vw, 480px)" }}>404
+          <div className='d-none d-md-inline' >❌</div>
+          <div className=' d-sm-inline d-md-none' style={{ textAlign: 'center' }} >🔎</div>
+          <div style={{ fontSize: "clamp(50px, 6vw, 200px)" }}>Página no encontrada</div>
+          <p style={{ fontSize: "clamp(20px, 2.4vw, 100px)" }}>La página que solicitaste no pudo ser encontrada</p>
+        </h1>
+      </div>
     </Container>
   )
 }
