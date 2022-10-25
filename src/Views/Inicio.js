979930431c55d14f4f68094e5b2262d89e3abb60
Martha <martha.fitz@jyreh.mx>
2022-10-25 11:31:00 -0500

    Cambios en Inicio y Productos hechos

--- src/Views/Inicio.js
+++ src/Views/Inicio.js
@@ -11,7 +11,7 @@ const Inicio = () => {
             <Image src={page} alt='img-page' style={{ objectFit: 'cover', height: '22rem' }} className='p-0' />
           <Card.ImgOverlay className='d-flex align-items-center p-5'>
             <Col className="p-0" >
-              <Card.Title className='animate__animated animate__jackInTheBox' style={{ fontSize: "2rem" }}>
+              <Card.Title className='animate__animated animate__pulse' style={{ fontSize: "2rem" }}>
                 Soluciones tecnológicas en inteligencia de negocios y el
                 manejo de datos.
               </Card.Title>
@@ -32,22 +32,25 @@ const Inicio = () => {
               </Card.Body>
             </Card>
           </Col>
+
           <Col style={{  borderBottom: 'solid #6BBBAE 1px' }} >
-            <Card className=" p-5" style={{  border:'none' }}>
-              <Card.Body className='animate__animated animate__fadeInUp'>
-                <Card.Title style={{ fontSize: '25px' }}>
-                  <i className="bi bi-heart-pulse m-3" style={{ fontSize: '45px', color: '#6BBBAE' }}></i>
-                  Salud
-                </Card.Title>
-                <Card.Text style={{ fontSize: '18px', textAlign: 'justify' }}>
-                  Desarrollo de sistemas de diagnóstico asistido por
-                  computadora y observatorios de salud, a partir de
-                  herramientas de visión por computadora e inteligencia
-                  artificial (IA).
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
+              <Card className="p-5 m-0" style={{  border:'none' }}>
+                <Card.Body className='animate__animated animate__fadeInUp'>
+                  <Card.Title className="mb-2" style={{ fontSize: '25px' }}>
+                    <i className="bi bi-building m-3 hvr-grow" aria-hidden="true" style={{ fontSize: '45px', color: '#6BBBAE' }}></i>
+                    Infraestructura
+                  </Card.Title>
+                  <Card.Text style={{ fontSize: '18px', textAlign: 'justify' }}>
+                    Implementación de modelos de IA y
+                    percepción remota para estandarizar los procesos de
+                    adquisición de datos y métricas cuantitativas aplicables a la
+                    supervisión, monitoreo y diagnóstico de activos de
+                    infraestructura pública y privada.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          
         </Row>
           <Row>
             <Col style={{  borderRight: 'solid #6BBBAE 1px' }}>
@@ -68,22 +71,21 @@ const Inicio = () => {
             </Col>
 
             <Col>
-              <Card className="p-5 m-0" style={{  border:'none' }}>
-                <Card.Body className='animate__animated animate__fadeInUp'>
-                  <Card.Title className="mb-2" style={{ fontSize: '25px' }}>
-                    <i className="bi bi-building m-3 hvr-grow" aria-hidden="true" style={{ fontSize: '45px', color: '#6BBBAE' }}></i>
-                    Infraestructura
-                  </Card.Title>
-                  <Card.Text style={{ fontSize: '18px', textAlign: 'justify' }}>
-                    Implementación de modelos de IA y
-                    percepción remota para estandarizar los procesos de
-                    adquisición de datos y métricas cuantitativas aplicables a la
-                    supervisión, monitoreo y diagnóstico de activos de
-                    infraestructura pública y privada.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
+            <Card className=" p-5" style={{  border:'none' }}>
+              <Card.Body className='animate__animated animate__fadeInUp'>
+                <Card.Title style={{ fontSize: '25px' }}>
+                  <i className="bi bi-heart-pulse m-3" style={{ fontSize: '45px', color: '#6BBBAE' }}></i>
+                  Salud
+                </Card.Title>
+                <Card.Text style={{ fontSize: '18px', textAlign: 'justify' }}>
+                  Desarrollo de sistemas de diagnóstico asistido por
+                  computadora y observatorios de salud, a partir de
+                  herramientas de visión por computadora e inteligencia
+                  artificial (IA).
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
           </Row>
       </Container>
     </>
