a3316f493e567bb3f8518eb70307211f0e7ce479
gonzaloronzon <gonzaloronzon@jyreh.mx>
2022-11-04 09:02:32 -0600

    footer sticky funcional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { Route, Routes, Navigate, useLocation } from "react-router-dom";
-import { useRef, useState, useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Inicio from "./Views/Inicio";
 import Nosotros from "./Views/Nosotros";
 import Productos from "./Views/Productos/Productos";
@@ -10,50 +9,23 @@ import Footer from "./Components/Footer";
 import "animate.css";
 
 function App() {
-  const refApp = useRef();
-  const refFooter = useRef();
-  const refBodyWrapper = useRef();
-  const [lugar, setLugar] = useState();
-
-  useEffect(() => {
-    let windowHeight = window.innerHeight;
-    let footerHeight = refFooter.current?.offsetHeight;
-    let appHeight = refApp.current?.offsetHeight;
-    let bodyWrapperHeight = appHeight - footerHeight;
-    console.log("body: " + bodyWrapperHeight);
-    if (appHeight <= 857) {
-      bodyWrapperHeight = windowHeight - footerHeight;
-    }
-    refBodyWrapper.current.style.height = `${bodyWrapperHeight}px`;
-    console.log(
-      "window: " +
-        windowHeight +
-        "\nfooter: " +
-        footerHeight +
-        "\napp: " +
-        appHeight +
-        "\nbody: " +
-        bodyWrapperHeight
-    );
-  }, []);
-
   return (
-    <div id="app" className="app" ref={refApp}>
-      <div id="body-wrapper" ref={refBodyWrapper}>
+      <div id="app" className="app">
+        <div id="body-wrapper">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/Inicio" element={<Navigate to="/" />} />
-          <Route path="/Nosotros" element={<Nosotros />} />
-          <Route path="/Productos" element={<Productos />} />
-          <Route path="/Contacto" element={<Contacto />} />
-          <Route path="*" element={<Pagina404 />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/Inicio" element={<Navigate to="/" />} />
+            <Route path="/Nosotros" element={<Nosotros />} />
+            <Route path="/Productos" element={<Productos />}/>
+            <Route path="/Contacto" element={<Contacto />}/>
+            <Route path="*" element={<Pagina404 />}/>
+          </Routes>
+        </div>
+        <div id="footer" className="footer">
+          <Footer />
+        </div>
       </div>
-      <div id="footer" ref={refFooter}>
-        <Footer />
-      </div>
-    </div>
   );
 }
 
