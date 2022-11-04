182637f614bc2f2aea7c9d224c5328798771bb59
gonzaloronzon <gonzaloronzon@jyreh.mx>
2022-11-03 18:02:29 -0600

    Version medio estable footer sticky

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import Inicio from './Views/Inicio';
-import Nosotros from './Views/Nosotros';
-import Productos from './Views/Productos/Productos';
-import Pagina404 from './Views/Pagina404';
-import Contacto from './Views/Contacto'; 
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import { useRef, useState, useEffect } from "react";
+import Inicio from "./Views/Inicio";
+import Nosotros from "./Views/Nosotros";
+import Productos from "./Views/Productos/Productos";
+import Pagina404 from "./Views/Pagina404";
+import Contacto from "./Views/Contacto";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import 'animate.css';
+import "animate.css";
 
 function App() {
+  const refApp = useRef();
+  const refFooter = useRef();
+  const refBodyWrapper = useRef();
+  const [lugar, setLugar] = useState();
+
+  useEffect(() => {
+    let windowHeight = window.innerHeight;
+    let footerHeight = refFooter.current?.offsetHeight;
+    let appHeight = refApp.current?.offsetHeight;
+    let bodyWrapperHeight = appHeight - footerHeight;
+    console.log("body: " + bodyWrapperHeight);
+    if (appHeight <= 857) {
+      bodyWrapperHeight = windowHeight - footerHeight;
+    }
+    refBodyWrapper.current.style.height = `${bodyWrapperHeight}px`;
+    console.log(
+      "window: " +
+        windowHeight +
+        "\nfooter: " +
+        footerHeight +
+        "\napp: " +
+        appHeight +
+        "\nbody: " +
+        bodyWrapperHeight
+    );
+  }, []);
+
   return (
-  <BrowserRouter> 
-      <Navbar/>
-      <div className="app">
-      <Routes>
-        <Route path='/' element={<Inicio/>}/> 
-        <Route path='/Inicio' element={<Navigate to='/'/>}/> 
-        <Route path='/Nosotros' element={<Nosotros/>}/> 
-        <Route path='/Productos' element={<Productos/>}/> 
-        <Route path='/Contacto' element={<Contacto/>}/> 
-        <Route path='*' element={<Pagina404/>}/> 
-      </Routes>
-      <Footer/>
+    <div id="app" className="app" ref={refApp}>
+      <div id="body-wrapper" ref={refBodyWrapper}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/Inicio" element={<Navigate to="/" />} />
+          <Route path="/Nosotros" element={<Nosotros />} />
+          <Route path="/Productos" element={<Productos />} />
+          <Route path="/Contacto" element={<Contacto />} />
+          <Route path="*" element={<Pagina404 />} />
+        </Routes>
       </div>
-  </BrowserRouter>
+      <div id="footer" ref={refFooter}>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
-
 export default App;
-
-
