770213e6d40e0048b115cb1c79c9f10b3bbcc7c0
Martha Fitz <martha.fitz@jyreh.mx>
2023-04-28 14:08:26 -0500

    Se cierra NavBar al seleccionar elementos de la barra de navegacion, se agrego global.json nuevo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,45 +1,120 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
-import Nav from 'react-bootstrap/Nav';
-import NavbarRB from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from '../images/logoVertical3.webp';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Nav from "react-bootstrap/Nav";
+import NavbarRB from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../images/logoVertical3.webp";
+import { NavLink } from "react-router-dom";
 import "../App.css";
-import "../css/Salud.css"
-import { useTranslation } from 'react-i18next';
+import "../css/Salud.css";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
 // Componente Navbar contiene la barra de navegacion de la pagina y el logotipo de Kaminoittech
 
 const Navbar = () => {
-  const [t] = useTranslation("global")
+  const [t] = useTranslation("global");
+
+  // Estado expanded se inicializa en false para que mantenga el navbar cerrado al recargar por primera vez
+  const [expanded, setExpanded] = useState(false);
+
+  // Esta es una función que alterna el valor de la variable "expanded".
+  const toggle = () => {
+    setExpanded(!expanded);
+  };
+
+  /**
+   La función `onSelect` cambia cuando `eventKey` es mayor o igual a 1.
+    * El parámetro eventKey es un valor que se pasa a la función onSelect cuando se desencadena un evento específico, como un clic o una pulsación de tecla. En el fragmento de código dado, la función onSelect comprueba si eventKey es mayor o igual a 1.
+   */
+  const onSelect = (eventKey) => {
+    if (eventKey >= 1) {
+      toggle();
+    }
+  };
+
   return (
     <>
-    {/* NavbarRB tiene todo el contenido  de la barra de navegación este fue obtenido de react-bootstrap con un fondo de color negro y funcional responsive para version moviles */}
-      <NavbarRB id="navbarStyle" collapseOnSelect expand="md" >
-        <Container className='p-0 py-2'> 
-          <NavbarRB.Brand className='mr-auto' to="/Inicio">
+      {/* NavbarRB tiene todo el contenido  de la barra de navegación este fue obtenido de react-bootstrap con un fondo de color negro y funcional responsive para version moviles */}
+      <NavbarRB
+        id="navbarStyle"
+        collapseOnSelect
+        expand="md"
+        onToggle={toggle}
+        expanded={expanded}
+        onSelect={onSelect}
+      >
+        <Container className="p-0 py-2">
+          <NavbarRB.Brand className="mr-auto" to="/Inicio">
             {/* Se le coloco un Nav.Link junto con una etiqueta image para colocar el logotipo y es redirijido a la pagina de Inicio  */}
-            <Nav.Link className='px-3' as={NavLink} to="/Inicio">
-             <Image fluid src={logo} alt='logo' style={{ maxWidth: "50vw"}} />
+            <Nav.Link className="px-3" as={NavLink} to="/Inicio">
+              <Image fluid src={logo} alt="logo" style={{ maxWidth: "50vw" }} />
             </Nav.Link>
           </NavbarRB.Brand>
           {/* Esta parte del código del navbar es cuando se usa en version movil, se colapsa el menu y se convierte en el menú de burger famoso para moviles */}
-          <NavbarRB.Toggle aria-controls="responsive-navbar-nav" className='m-3' />
-          <NavbarRB.Collapse className='flex-grow-0 ' id="responsive-navbar-nav">
+          <NavbarRB.Toggle
+            aria-controls="responsive-navbar-nav"
+            className="m-3"
+          />
+          <NavbarRB.Collapse
+            className="flex-grow-0 "
+            id="responsive-navbar-nav"
+          >
             {/*  */}
-            <Nav className='p-3'>
-              <Nav.Link as={NavLink} to="/Inicio" className='text-white hvr-underline-from-left-modificado'>{t("Navbar.Home")}</Nav.Link>
-              <Nav.Link as={NavLink} to="/Nosotros" className='text-white hvr-underline-from-left-modificado'>{t("Navbar.Us")}</Nav.Link>
+            {/* <Offcanvas.Body></Offcanvas.Body> */}
+            <Nav className="p-3">
+              <Nav.Link
+                eventKey={1} 
+                as={NavLink}
+                to="/Inicio"
+                className="text-white hvr-underline-from-left-modificado"
+              >
+                {t("Navbar.Home")}
+              </Nav.Link>
+              <Nav.Link
+                eventKey={2} 
+                as={NavLink}
+                to="/Nosotros"
+                className="text-white hvr-underline-from-left-modificado"
+              >
+                {t("Navbar.Us")}
+              </Nav.Link>
               {/*<Nav.Link as={NavLink} to="/Productos" className='text-white hvr-underline-from-left-modificado'>{t("Navbar.Products")}</Nav.Link>*/}
-              <NavDropdown title={t("Navbar.Products")} className='text-white' id='navTitle'>
-                <NavDropdown.Item  as={NavLink} to="/Infraestructura" id='product'>{t("General.Title1")}</NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/Retail" id='product'>{t("General.Title2")}</NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/Salud" id='product'>{t("General.Title3")}</NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/Movilidad" id='product'>{t("General.Title4")}</NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/Banca" id='product'>{t("General.Title5")}</NavDropdown.Item>
+              <NavDropdown
+                eventKey={3} 
+                title={t("Navbar.Products")}
+                className="text-white"
+                id="navTitle"
+              >
+                <NavDropdown.Item
+                  eventKey={3.1} 
+                  as={NavLink}
+                  to="/Infraestructura"
+                  id="product"
+                >
+                  {t("General.Title1")}
+                </NavDropdown.Item>
+                <NavDropdown.Item eventKey={3.2} as={NavLink} to="/Retail" id="product">
+                  {t("General.Title2")}
+                </NavDropdown.Item>
+                <NavDropdown.Item eventKey={3.3} as={NavLink} to="/Salud" id="product">
+                  {t("General.Title3")}
+                </NavDropdown.Item>
+                <NavDropdown.Item eventKey={3.4} as={NavLink} to="/Movilidad" id="product">
+                  {t("General.Title4")}
+                </NavDropdown.Item>
+                <NavDropdown.Item eventKey={3.5} as={NavLink} to="/Banca" id="product">
+                  {t("General.Title5")}
+                </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link as={NavLink} to="/Contacto" className='text-white hvr-underline-from-left-modificado'>{t("Navbar.Contact")}</Nav.Link>
+              <Nav.Link
+                eventKey={4}
+                as={NavLink}
+                to="/Contacto"
+                className="text-white hvr-underline-from-left-modificado"
+              >
+                {t("Navbar.Contact")}
+              </Nav.Link>
               {/* <Nav.Link as={NavLink} to="/Carreras" className='text-white hvr-underline-from-left-modificado'>{t("Navbar.Carrers")}</Nav.Link> */}
             </Nav>
           </NavbarRB.Collapse>
@@ -47,5 +122,5 @@ const Navbar = () => {
       </NavbarRB>
     </>
   );
-}
+};
 export default Navbar;
